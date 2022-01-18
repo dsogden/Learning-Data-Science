4a349c65ad8cc5d8366ebcbce5ea6290e6434e25
Dylan Ogden <67968474+dsogden@users.noreply.github.com>
2022-01-18 17:05:08 -0600

    Update and rename 0.tcl to Main.tcl

--- Main.tcl
+++ Main.tcl
@@ -2,7 +2,7 @@ mol new ../step3_input.psf
 mol addfile ../step3_input.pdb
 
 mol new ../step3_input.psf
-mol addfile ../output/0/replica.0.dcd type dcd first 0 last -1 filebonds 1 autobonds 1 waitfor all
+mol addfile ../output/$replica/replica.$replica.dcd type dcd first 0 last -1 filebonds 1 autobonds 1 waitfor all
 
 source functions.tcl
 source calculate.tcl
